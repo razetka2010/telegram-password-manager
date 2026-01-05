80636726f0a73b9bbb8318320360d17c9af0a89c
razetka <128233067+razetka2010@users.noreply.github.com>
2026-01-05 13:56:03 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,4 +1,3 @@
-<?php
 class Database {
     private $host = "dpg-d5dd0b4hg0os73f6lpkg-a";
     private $db_name = "telegram-password-db";
@@ -10,16 +9,14 @@ class Database {
         $this->conn = null;
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                "pgsql:host=" . $this->host . ";port=5432;dbname=" . $this->db_name,
                 $this->username,
                 $this->password
             );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
         return $this->conn;
     }
 }
-?>
